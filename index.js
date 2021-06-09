f6c62c4a5a66c409f3adb783f848b65d8892448d
Ronnie Song <ronniesong0809@gmail.com>
2021-06-09 14:48:29 -0700

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const hostname = "localhost";
+const port = "8080";
+
+app.get("/foo", (req, res) => {
+  res.send("bar");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
