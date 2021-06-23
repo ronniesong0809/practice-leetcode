4e66902e6062021a5bb5ddd0c67ad50b5cf5ea36
Ronnie Song <ronniesong0809@gmail.com>
2021-06-23 11:16:30 -0700

    add sort order in the result set

--- apis/getAllCompanies.js
+++ apis/getAllCompanies.js
@@ -2,7 +2,7 @@ const companiesModel = require("../models/companies");
 
 const getAllCompanies = () => {
   try {
-    return companiesModel.find();
+    return companiesModel.find().sort({ count: -1 });
   } catch (error) {
     console.error(`getAllCompanies error: ${error}`);
   }
